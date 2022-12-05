7d2188060321ab12020bf66485b302d41b1d8381
Andrii Nahornyi <108332521+AndriiNahornyi@users.noreply.github.com>
2022-12-05 12:45:14 +0200

    adds joi

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,67 @@
-const express = require('express')
+const express = require("express");
+// const Joi = require("joi");
+const router = express.Router();
 
-const router = express.Router()
+router.get("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  //   const schema = Joi.object({
+  //     username: Joi.string()
+  //         .alphanum()
+  //         .min(3)
+  //         .max(30)
+  //         .required(),
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  //     password: Joi.string()
+  //         .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  //     repeat_password: Joi.ref('password'),
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  //     access_token: [
+  //         Joi.string(),
+  //         Joi.number()
+  //     ],
 
-module.exports = router
+  //     birth_year: Joi.number()
+  //         .integer()
+  //         .min(1900)
+  //         .max(2013),
+
+  //     email: Joi.string()
+  //         .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+  // })
+  //     .with('username', 'birth_year')
+  //     .xor('password', 'access_token')
+  //     .with('password', 'repeat_password');
+
+  // schema.validate({ username: 'abc', birth_year: 1994 });
+  // // -> { value: { username: 'abc', birth_year: 1994 } }
+
+  // schema.validate({});
+  // // -> { value: {}, error: '"username" is required' }
+
+  // // Also -
+
+  // try {
+  //     const value = await schema.validateAsync({ username: 'abc', birth_year: 1994 });
+  // }
+  // catch (err) { }
+
+  res.json({ message: "template message" });
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
