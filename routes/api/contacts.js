ea89701c680f7aa90b3d47ae2288b46de65432cf
Andrii Nahornyi <108332521+AndriiNahornyi@users.noreply.github.com>
2022-12-07 15:39:24 +0200

    adds task solution

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,67 +1,94 @@
 const express = require("express");
-// const Joi = require("joi");
+const Joi = require("joi");
+const contacts = require("../../models/contacts");
+
+const { createError } = require("../../helpers");
+
 const router = express.Router();
 
+const contactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string()
+    .pattern(
+      /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+    )
+    .message("Phone number must be min 6 numbers length")
+    .required(),
+});
+
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw createError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  //   const schema = Joi.object({
-  //     username: Joi.string()
-  //         .alphanum()
-  //         .min(3)
-  //         .max(30)
-  //         .required(),
-
-  //     password: Joi.string()
-  //         .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-
-  //     repeat_password: Joi.ref('password'),
-
-  //     access_token: [
-  //         Joi.string(),
-  //         Joi.number()
-  //     ],
-
-  //     birth_year: Joi.number()
-  //         .integer()
-  //         .min(1900)
-  //         .max(2013),
-
-  //     email: Joi.string()
-  //         .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-  // })
-  //     .with('username', 'birth_year')
-  //     .xor('password', 'access_token')
-  //     .with('password', 'repeat_password');
-
-  // schema.validate({ username: 'abc', birth_year: 1994 });
-  // // -> { value: { username: 'abc', birth_year: 1994 } }
-
-  // schema.validate({});
-  // // -> { value: {}, error: '"username" is required' }
-
-  // // Also -
-
-  // try {
-  //     const value = await schema.validateAsync({ username: 'abc', birth_year: 1994 });
-  // }
-  // catch (err) { }
-
-  res.json({ message: "template message" });
+  try {
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+
+    const result = await contacts.removeContact(contactId);
+    if (!result) {
+      throw createError(404, "Not found");
+    }
+    res.json({
+      message: "Contact deleted",
+    });
+  } catch (error) {
+    console.log("error", error);
+    next(error);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
+    const { contactId } = req.params;
+    const result = await contacts.updateContact(contactId, req.body);
+    if (!result) {
+      throw createError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
