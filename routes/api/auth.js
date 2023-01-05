1096e0589c5355a6a93efd1e2492ea8dd7b55dae
Andrii Nahornyi <108332521+AndriiNahornyi@users.noreply.github.com>
2023-01-05 14:03:19 +0200

    adds task solution

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,8 +6,9 @@ const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
+const { nanoid } = require("nanoid");
 
-const { createError, createHashPassword } = require("../../helpers");
+const { createError, createHashPassword, sendMail } = require("../../helpers");
 
 const User = require("../../models/user");
 const { authorize, upload } = require("../../middlewares");
@@ -22,6 +23,13 @@ const registerUserSchema = Joi.object({
 
 const signInUserSchema = Joi.object({
   password: Joi.string().min(6).required(),
+  email: Joi.string()
+    // eslint-disable-next-line no-useless-escape
+    .pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+    .required(),
+});
+
+const verifyUserSchema = Joi.object({
   email: Joi.string()
     // eslint-disable-next-line no-useless-escape
     .pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
@@ -47,18 +55,72 @@ router.post("/signup", async (req, res, next) => {
 
     const hashPassword = await createHashPassword(password);
     const avatarURL = gravatar.url(email);
+    const verificationToken = nanoid();
 
     const newUser = await User.create({
       email,
       password: hashPassword,
       avatarURL,
+      verificationToken,
     });
+
+    const mail = {
+      to: email,
+      subject: "Email verification",
+      html: `<a href='http://localhost:3000/api/auth/verify/${verificationToken}'>Verify user</a>`,
+    };
+
+    await sendMail(mail);
+
     res.status(201).json({
       user: {
         email: newUser.email,
         subscription: newUser.subscription,
       },
     });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/verify/:verificationToken", async (req, res, next) => {
+  try {
+    const { verificationToken } = req.params;
+    const user = await User.findOne({ verificationToken });
+    if (!user) {
+      throw createError(404, "User not found");
+    }
+    await User.findByIdAndUpdate(user._id, {
+      verify: true,
+      verificationToken: "",
+    });
+    res.json({ message: "Verification successful" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/verify", async (req, res, next) => {
+  try {
+    const { error } = verifyUserSchema.validate(req.body);
+    if (error) {
+      throw createError(400, "error.message");
+    }
+    const { email } = req.body;
+    const user = await User.findOne(email);
+    if (!user) {
+      throw createError(404, "User not found");
+    }
+    if (user.verify) {
+      throw createError(400, "Verification has already been passed");
+    }
+    const mail = {
+      to: email,
+      subject: "Email verification",
+      html: `<a href='http://localhost:3000/api/auth/verify/${user.verificationToken}'>Verify user</a>`,
+    };
+    await sendMail(mail);
+    res.json({ message: "Verification email sent" });
   } catch (error) {
     next(error);
   }
@@ -82,6 +144,10 @@ router.post("/signin", async (req, res, next) => {
     if (!isValidPassword) {
       throw createError(401, "Credentials are wrong");
     }
+    if (newUser.verificationToken === "false") {
+      throw createError(401, "User not verified");
+    }
+
     const payload = { id: newUser.id };
 
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
@@ -106,7 +172,7 @@ router.get("/logout", authorize, async (req, res, next) => {
     const { _id } = req.user;
     await User.findByIdAndUpdate(_id, { token: "" });
     res.json({
-      message: "Logout successfull",
+      message: "Logout successful",
     });
   } catch (error) {
     next(error);
